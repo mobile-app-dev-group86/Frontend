93ef504cfd91f4ec82c00ffee383d2363c116be2
GabbieAizal001 <gabriellelazia@gmail.com>
2025-05-17 13:34:54 +0000

    Add expo project files

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, this is my first Expo app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
