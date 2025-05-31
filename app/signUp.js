8402521592e7b0a838474465f8152d161b8bf968
GabbieAizal001 <gabriellelazia@gmail.com>
2025-05-31 12:47:06 +0000

    Add Welcome and SignUp screens with assets

---
+++ app/signUp.js
@@ -0,0 +1,257 @@
+import react, {useState} from 'react';
+import {View,Text,TextInput,TouchableOpacity,StyleSheet,Image,Pressable} from 'react-native';
+import googleLogo from '../assets/images/google.png';
+import facebookLogo from '../assets/images/facebook.png';
+
+const signUpScreen =()=>{
+    const[userName,setUserName]=useState('');
+    const[password,setPassword]=useState('');
+    
+    const [showPassword, setShowPassword] = useState(false);
+    const[confirmPassword,setConfirmPassword]=useState('');
+    const [showConfirmPassword, setShowConfirm] = useState(false);
+    const [checked, setChecked] = useState(false);
+
+    const mismatch=()=>{
+        if(password!==confirmPassword){
+            alert("Password mismatch");
+            return;
+        }
+        console.log("Signing up with:",userName,password)
+    };
+    return(
+        <View style={styles.container}>
+            <View style={{height:'5%'}}></View>
+    
+            <Text style={styles.title}>Sign Up</Text>
+           <Text>Username</Text>
+            <TextInput
+            style={styles.input}
+            autoCapitalize='none'
+            keyboardType='text'
+            value={userName}
+            onChangeText={setUserName}
+            />
+ 
+           <Text>Password</Text>
+          <View style={styles.passwordContainer}>           
+            <TextInput
+            style={styles.passwordInput}
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize='none'
+            />
+
+           <TouchableOpacity
+        onPress={() => setShowPassword(!showPassword)}
+        style={styles.eyeButton}
+      >
+        <Image
+          source={
+            showPassword
+              ? require('../assets/images/hide.png') 
+              : require('../assets/images/view.png')     
+          }
+          style={styles.eyeIcon}
+        />
+      </TouchableOpacity>
+      </View>
+
+
+     
+
+            
+
+            <Text>Confirm Password</Text>
+            <View style={styles.passwordContainer}>
+            <TextInput
+            style={styles.passwordInput}
+            autoCapitalize='none'
+            secureTextEntry={!showConfirmPassword}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            />
+
+             <TouchableOpacity
+        onPress={() => setShowConfirm(!showConfirmPassword)}
+        style={styles.eyeButton}
+      >
+        <Image
+          source={
+            showConfirmPassword
+              ? require('../assets/images/hide.png') 
+              : require('../assets/images/view.png')     
+          }
+          style={styles.eyeIcon}
+        />
+      </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.checkboxContainer}>
+      <Pressable
+        onPress={() => setChecked(!checked)}
+        style={[styles.checkbox, checked && styles.checked]}
+      >
+        {checked && <Text style={styles.checkmark}>✓</Text>}
+      </Pressable>
+      <Text style={styles.label}>I agree to the Terms and Conditions</Text>
+    </View>
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Sign Up</Text>
+
+            </TouchableOpacity>
+           
+              <View style={styles.dividerContainer}>
+                  <View style={styles.line} />
+                  <Text style={styles.dividerText}>Or continue with</Text>
+                  <View style={styles.line} />
+
+               </View>
+          <View style={{alignItems: 'center',flexDirection: 'row',justifyContent: 'center',gap:20}}>
+            <View style={styles.circle}> <Image source={googleLogo} style={{width:"30", height:'30',resizeMode:'contain'}}></Image></View>
+            <View style={styles.circle}><Image source={facebookLogo} style={{width:"50", height:'50'}}></Image></View>
+          </View>
+
+          <View style={{alignItems:'center',marginTop:10}}>
+            <Text>Already have an account? Sign In</Text>
+          </View>
+
+        </View>
+
+        
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 24,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#999',
+    borderRadius: 10,
+    padding: 12,
+    marginBottom: 12,
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 12,
+  },
+  googleButton: {
+    backgroundColor: '#db4437',
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 12,
+  },
+  facebookButton: {
+    backgroundColor: '#4267B2',
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  or: {
+    textAlign: 'center',
+    marginVertical: 12,
+    color: '#999',
+  },
+
+
+dividerContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginVertical: 20,
+},
+
+line: {
+  flex: 1,
+  height: 1,
+  backgroundColor: '#ccc',
+},
+
+dividerText: {
+  marginHorizontal: 10,
+  color: '#999',
+},
+
+passwordContainer: {
+    position: 'relative',
+    marginBottom: 15,
+  },
+  passwordInput: {
+    padding: 12,
+    paddingRight: 45,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#ccc',
+    fontSize: 16,
+  },
+  eyeButton: {
+    position: 'absolute',
+    right: 12,
+    top: '30%',
+  },
+  eyeIcon: {
+    width: 24,
+    height: 24,
+  },
+
+checkboxContainer: {
+  flexDirection: 'row',     
+  alignItems: 'center',     
+  marginTop: 10,
+  marginBottom: 20,
+},
+
+checkbox: {
+  width: 24,
+  height: 24,
+  borderWidth: 1,
+  borderColor: '#999',
+  borderRadius: 4,
+  marginRight: 8,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+
+checked: {
+  backgroundColor: 'green',  
+},
+
+checkmark: {
+  color: '#fff',               
+  fontWeight: 'bold',
+},
+
+circle: {
+  width: 50,
+  height: 50,
+  borderRadius: 25,
+  borderWidth: 1,               
+  borderColor: '#333',     
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+
+});
+
+
+export default signUpScreen;
---
+++ app/welcomeScreen.js
@@ -0,0 +1,80 @@
+
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import logo from '../assets/images/chatterlylogo.jpg';
+
+export default function WelcomeScreen() {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <View style={{ marginTop: 200 }}>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
+      </View>
+
+      <View>
+        <Text style={styles.welcome}>Where conversations feel alive...</Text>
+      </View>
+
+      <View style={{ flex: 0.85 }} />
+
+      <View
+        style={[
+          styles.buttonContainer,
+          pressed && styles.buttonPressed,
+        ]}
+      >
+        <Link href="/signUp" asChild>
+          <Pressable
+            onPressIn={() => setPressed(true)}
+            onPressOut={() => setPressed(false)}
+          >
+          
+
+    
+            
+            <Text style={styles.buttonText}>Get Started</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  logo: {
+    width: 350,
+    height: 150,
+  },
+  welcome: {
+    fontSize: 16,
+    color: '#000',
+    fontStyle: 'italic',
+    textAlign: 'center',
+    marginTop: 30, 
+  },
+  buttonContainer: {
+    backgroundColor: 'green',
+    width: '70%',
+    paddingVertical: 14,
+    alignItems: 'center',
+    borderRadius: 25,
+    alignSelf: 'center',
+    
+  },
+  buttonPressed: {
+    backgroundColor: '#004d00', 
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white',
+  },
+});
